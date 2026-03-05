0498e7e5b68eb5457796643f8594baf950e8f71b
Matt Oestreich <matthewpoestreich@gmail.com>
2026-03-04 21:07:22 -0600

    fix error

--- src/value/error.rs
+++ src/value/error.rs
@@ -5,13 +5,18 @@ use std::{error, fmt};
 pub enum Error {
     Converting { from: Value, to: String },
     Parsing { value: String },
+    ImproperlyFloat,
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Error::Converting { from, to } => write!(f, "Cannot convert {:?} to {:?}", from, to),
+            Error::Converting { from, to } => write!(f, "cannot convert {:?} to {:?}", from, to),
             Error::Parsing { value } => write!(f, "'{value}' cannot be parsed as Value"),
+            Error::ImproperlyFloat => write!(
+                f,
+                "attempted to perform an operation which only makes sense for integers, but value is currently a float"
+            ),
         }
     }
 }
